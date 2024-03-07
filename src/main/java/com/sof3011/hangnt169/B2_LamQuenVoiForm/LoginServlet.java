298f6a659981ee69f0058809a14ddbd088f2e6f9
HangNT169 <HangNT169@fpt.edu.vn>
2024-03-07 16:07:07 +0700

    Buổi 2: Làm việc với form

---
+++ src/main/java/com/sof3011/hangnt169/B2_LamQuenVoiForm/LoginServlet.java
@@ -0,0 +1,42 @@
+package com.sof3011.hangnt169.B2_LamQuenVoiForm;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+// value => duong dan va bat dau bang dau /. Neu k tomcat k build
+// Duong dan giong y de bai
+// BE & FE => tang trung gian: controller va trao doi thong qua phuong thuc HTTP.
+// Co 4 loai HTTP Methods co ban:
+// GET : Lay ra , Hien thi <=> READ
+// POST : Add nha <=> CREATE
+// PUT : Update
+// DELETE : DELETE
+// J4, J5: Chi lam viec vs 2 loai get , post
+// Get: Lay ra, hien thi
+// Post: Xu ly
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Truyen gia tri cua 1 bien tu servlet sang jsp => setAttribute demo
+        String mess = "HangNT169";
+        request.setAttribute("a",mess);
+        // Chuyen trang
+        // C1:
+        request.getRequestDispatcher("fomr-login.jsp").forward(request,response);
+        // C2:
+//        response.sendRedirect("fomr-login.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
