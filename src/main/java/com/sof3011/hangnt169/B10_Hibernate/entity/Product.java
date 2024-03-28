fd9d11beb53c45e65f3cafef1c979bd9decd7703
HangNT169 <HangNT169@fpt.edu.vn>
2024-03-28 14:47:21 +0700

    Buổi 11: Hibernate - phần 2 - join bảng

--- src/main/java/com/sof3011/hangnt169/B10_Hibernate/entity/Product.java
+++ src/main/java/com/sof3011/hangnt169/B10_Hibernate/entity/Product.java
@@ -1,4 +1,41 @@
 package com.sof3011.hangnt169.B10_Hibernate.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "product_code")
+    private String productCode;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "price")
+    private float price;
+
+    @Column(name = "description")
+    private String description;
+
+    // 1 product -> 1 category
+    // 1 category -> N product
+    // => N: product
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category1 category1;
+
 }
+
